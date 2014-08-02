986103e88abbea0a6afcd57833aade5b9fd46847
dannyti <dannyti@users.noreply.github.com>
2014-08-02 21:42:11 +0530

    Update sbfrmsc-dti-14.04.sh

--- sbfrmsc-dti-14.04.sh
+++ sbfrmsc-dti-14.04.sh
@@ -5,11 +5,11 @@
 #
 # The Seedbox From Scratch Script
 #   By Notos ---> https://github.com/Notos/
-#
+#     Modified by dannyti ---> https://github.com/dannyti/
 #
 ######################################################################
 #
-#  Copyright (c) 2013 Notos (https://github.com/Notos/) 
+#  Copyright (c) 2013 Notos (https://github.com/Notos/) & dannyti (https://github.com/dannyti/)
 #
 #  Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions: 
 #
@@ -162,7 +162,7 @@ function getString
     echo "#"
     echo "# The Seedbox From Scratch Script"
     echo "#   By Notos ---> https://github.com/Notos/"
-    echo "#"
+    echo "#   Modified by dannyti ---> https://github.com/dannyti/"
     echo "#"
     echo "#"
     echo
