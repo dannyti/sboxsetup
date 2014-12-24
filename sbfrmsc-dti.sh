853f9ffaa35a819da93b293ef127199aaa0d17b3
dannyti <dannyti@users.noreply.github.com>
2014-12-24 16:06:39 +0530

    Update sbfrmsc-dti.sh

--- sbfrmsc-dti.sh
+++ sbfrmsc-dti.sh
@@ -483,9 +483,10 @@ mkdir -p /etc/ssl/private/
 openssl req -x509 -nodes -days 365 -newkey rsa:1024 -keyout /etc/ssl/private/vsftpd.pem -out /etc/ssl/private/vsftpd.pem -config /etc/seedbox-from-scratch/ssl/CA/caconfig.cnf
 
 if [ "$OS1" = "Debian" ]; then
+  apt-get purge -y --force-yes vsftpd
   echo "deb http://ftp.cyconet.org/debian wheezy-updates main non-free contrib" >> /etc/apt/sources.list.d/wheezy-updates.cyconet.list
   apt-get update
-  apt-get install -y -o Aptitude::Cmdline::ignore-trust-violations=true -t wheezy-updates debian-cyconet-archive-keyring vsftpd
+  apt-get install -y --force-yes -t wheezy-updates debian-cyconet-archive-keyring vsftpd
 else
   apt-get --yes install libcap-dev libpam0g-dev libwrap0-dev
 fi
