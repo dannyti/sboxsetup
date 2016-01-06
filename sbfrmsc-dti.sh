ed762915a315ce12c6dd98fc782ed83dc318745d
dannyti <dannyti@users.noreply.github.com>
2016-01-06 16:22:35 +0530

    Update sbfrmsc-dti.sh

--- sbfrmsc-dti.sh
+++ sbfrmsc-dti.sh
@@ -852,6 +852,8 @@ tar -xvf unrarsrc-5.3.8.tar.gz
 cd unrar
 sudo make -f makefile
 sudo install -v -m755 unrar /usr/bin
+cd ..
+rm -R unrar
 rm unrarsrc-5.3.8.tar.gz
 
 if [ "$OSV11" = "8" ]; then
@@ -872,10 +874,10 @@ echo ""
 echo "Your Login info can also be found at https://$IPADDRESS1/private/SBinfo.txt"
 echo "Download Data Directory is located at https://$IPADDRESS1/private "
 echo "To install ZNC, run installZNC from ssh as main user"
-echo "System will reboot now, but don't close this window until you take note of the port number: $NEWSSHPORT1"
+#echo "System will reboot now, but don't close this window until you take note of the port number: $NEWSSHPORT1"
 echo ""
-echo ""
+echo -e "\033[0;32;148mPlease login as main user and only then close this Window\033[39m"
 
-reboot
+#reboot
 
 ##################### LAST LINE ###########
