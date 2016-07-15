1065229ad7707a84620b806f662bcb65233aca8b
dannyti <dannyti@users.noreply.github.com>
2016-07-15 21:37:42 +0530

    Added .avi

--- mtn.sh
+++ mtn.sh
@@ -8,3 +8,6 @@ done;
 for i in *.mp4;
 do mtn "${i}" -f /usr/share/fonts/truetype/liberation/LiberationMono-Bold.ttf -c 3 -r 3 -s 300;
 done;
+for i in *.avi;
+do mtn "${i}" -f /usr/share/fonts/truetype/liberation/LiberationMono-Bold.ttf -c 3 -r 3 -s 300;
+done;
